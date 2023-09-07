308814175e2480330c670bea8d756ec26c5597f3
cocktailpeanut <cocktailpeanuts@proton.me>
2023-09-07 03:39:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: Coqui Bark Voice Cloning
+emoji: 🐸🐶
+colorFrom: yellow
+colorTo: gray
+sdk: gradio
+sdk_version: 3.42.0
+app_file: app.py
+pinned: false
+---
+
+Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
---
+++ pinokio.js
@@ -0,0 +1,34 @@
+const os = require('os')
+const fs = require('fs')
+const path = require("path")
+const exists = async p => !!(await fs.promises.stat(p).catch(e => false));
+module.exports = {
+  title: "Bark Voice Cloning",
+  icon: "icon.png",
+  description: "Upload a clean 20 seconds WAV file of the vocal persona you want to mimic, type your text-to-speech prompt and hit submit! A local version of https://huggingface.co/spaces/fffiloni/instant-TTS-Bark-cloning",
+  menu: async (kernel) => {
+    let installed = await exists(path.resolve(__dirname, "env"))
+    if (installed) {
+      let session = (await kernel.loader.load(path.resolve(__dirname, "session.json"))).resolved
+      return [{
+        when: "start.json",
+        on: `<i class='fa-solid fa-spin fa-circle-notch'></i> Running`,
+        type: "label",
+      }, {
+        when: "start.json",
+        off: "<i class='fa-solid fa-power-off'></i> Start",
+        href: "start.json?fullscreen=true&run=true",
+      }, {
+        when: "start.json",
+        on: "<i class='fa-solid fa-rocket'></i> Open UI",
+        href: (session && session.url ? session.url : "http://127.0.0.1:7860"),
+        target: "_blank"
+      }]
+    } else {
+      return [{
+        html: '<i class="fa-solid fa-plug"></i> Install',
+        href: "install.json?run=true&fullscreen=true"
+      }]
+    }
+  }
+}
